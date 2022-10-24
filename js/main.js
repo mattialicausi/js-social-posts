faea22fafdb154489e03bc785fb7e9f9ed11b983
mattialicausi <licausivitomattia@gmail.com>
2022-10-24 14:18:57 +0200

    creato funzione per popolare dinamicamente il dom utilizzando i valori dell'oggetto posts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+ CONSEGNA ESERCIZIO
+*/
+
 const posts = [
     {
         "id": 1,
@@ -57,3 +61,55 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+//prendo elementi da HTML-------------------------------------------------------------------------
+const contenitoreHTML = document.getElementById('container');
+
+//FUNZIONI-------------------------------------------------------------------------
+
+//funzione peer creare dinamicamente le card
+
+function drawCard(){
+    posts.forEach((value, index) =>{
+        const card = document.createElement('div');
+
+        card.innerHTML = `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${value.author.image}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${value.author.name}</div>
+                    <div class="post-meta__time">${value.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${value.content}</div>
+        <div class="post__image">
+            <img src="${value.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${value.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+        `;
+        
+       contenitoreHTML.append(card);
+    }) 
+}
+
+//RICHIAMO FUNZIONI-------------------------------------------------------------------------
+drawCard();
